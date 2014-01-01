cf4a336bcc692d083fac9d6d9ec7f5b7cb9b335b
ST-DDT <ST-DDT@gmx.de>
2014-01-01 17:38:04 +0100

    Secure CompatibilityLoader against uptodate Bukkit versions

--- src/de/st_ddt/crazyutil/compatibility/CompatibilityLoader.java
+++ src/de/st_ddt/crazyutil/compatibility/CompatibilityLoader.java
@@ -1,23 +1,34 @@
 package de.st_ddt.crazyutil.compatibility;
 
 import java.io.File;
-import java.net.MalformedURLException;
+import java.lang.reflect.Method;
 import java.net.URL;
+import java.net.URLClassLoader;
 
 import org.bukkit.Bukkit;
 import org.bukkit.Server;
 import org.bukkit.plugin.Plugin;
-import org.bukkit.plugin.java.PluginClassLoader;
 
 import de.st_ddt.crazyutil.resources.ResourceHelper;
 
 public class CompatibilityLoader
 {
 
+	private final static Method addUrl;
 	private final static String implementation;
 	private final static String version;
 	static
 	{
+		final Class<URLClassLoader> classLoaderClass = URLClassLoader.class;
+		Method _addUrl = null;
+		try
+		{
+			_addUrl = classLoaderClass.getDeclaredMethod("addURL", new Class[] { URL.class });
+			_addUrl.setAccessible(true);
+		}
+		catch (final Throwable t)
+		{}
+		addUrl = _addUrl;
 		final Server server = Bukkit.getServer();
 		final Class<? extends Server> serverClass = server.getClass();
 		final String serverClassName = serverClass.getName();
@@ -53,28 +64,44 @@ public class CompatibilityLoader
 		final File target = new File(plugin.getDataFolder(), "/compatibility/" + implementation + "/" + version + fileSuffix + ".jar");
 		if (!target.exists())
 			ResourceHelper.saveResource(plugin, "/compatibility/" + implementation + "/" + version + fileSuffix + ".jar", target);
+		final String className = packagePrefix + implementation + "." + version + packageSuffix + ".CompatibilityProvider";
 		try
 		{
-			loadClassContainer(plugin, target);
-			Class.forName(packagePrefix + implementation + "." + version + packageSuffix + ".CompatibilityProvider", true, plugin.getClass().getClassLoader());
-			System.err.println("[" + plugin.getName() + "] Loaded compatibility provider.");
+			attachClassContainer(plugin, target);
+			loadClass(plugin, className);
+			System.out.println("[" + plugin.getName() + "] Loaded compatibility provider.");
 			return true;
+		}
+		catch (final ClassNotFoundException e)
+		{
+			System.err.println("[" + plugin.getName() + "] Could not find matching compatibility provider!");
+			return false;
 		}
 		catch (final Throwable t)
 		{
 			System.err.println("[" + plugin.getName() + "] Error loading compatibility provider!");
-			System.err.println(t.getMessage());
+			System.err.println("[" + plugin.getName() + "] WARNING: Serious Bug detected, please report this!");
+			System.err.println("CompatibilityProvider: " + className);
+			t.printStackTrace();
 			return false;
 		}
 	}
 
-	protected static void loadClassContainer(final Plugin plugin, final File file) throws MalformedURLException
+	protected static void attachClassContainer(final Plugin plugin, final File file) throws Exception
 	{
 		if (!file.exists())
 			return;
-		final ClassLoader loader = plugin.getClass().getClassLoader();
+		final ClassLoader classLoader = plugin.getClass().getClassLoader();
 		final URL url = file.toURI().toURL();
-		((PluginClassLoader) loader).addURL(url);
+		if (addUrl == null)
+			throw new IllegalStateException("URLClassLoader.addURL method not found!");
+		else
+			addUrl.invoke(classLoader, url);
+	}
+
+	protected static void loadClass(final Plugin plugin, final String className) throws ClassNotFoundException
+	{
+		Class.forName(className, true, plugin.getClass().getClassLoader());
 	}
 
 	private CompatibilityLoader()

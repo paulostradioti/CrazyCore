2b0d7abafe90630cac7c3bf553ae669640019b43
ST-DDT <ST-DDT@gmx.de>
2014-02-12 22:02:51 +0100

    Improved ExtendedConditionHelper

--- src/main/java/de/st_ddt/crazyutil/conditions/ExtendedConditionHelper.java
+++ src/main/java/de/st_ddt/crazyutil/conditions/ExtendedConditionHelper.java
@@ -8,10 +8,19 @@ public class ExtendedConditionHelper extends ConditionHelper
 
 	public static Condition secure(final Condition condition, final int index, final Class<?> clazz, final Map<Integer, ? extends Collection<Class<?>>> classes)
 	{
+		if (condition == null)
+			return new Condition_TRUE();
 		final Collection<Class<?>> givenClasses = classes.get(index);
 		for (final Class<?> givenClass : givenClasses)
 			if (clazz.isAssignableFrom(givenClass))
 				return condition;
 		return new Condition_Class(index, clazz, condition);
+	}
+
+	public static Condition fastSecure(final Condition condition, final Class<?>... classes)
+	{
+		if (condition == null)
+			return new Condition_TRUE();
+		return condition.secure(fastParameterClasses(classes));
 	}
 }

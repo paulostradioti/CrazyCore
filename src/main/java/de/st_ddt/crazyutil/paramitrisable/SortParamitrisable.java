2a119544fd61c7bd9e9814078be07e4b5e55eb1d
ST-DDT <ST-DDT@gmx.de>
2014-05-01 10:48:04 +0200

    Improved SortParamitrisable

--- src/main/java/de/st_ddt/crazyutil/paramitrisable/SortParamitrisable.java
+++ src/main/java/de/st_ddt/crazyutil/paramitrisable/SortParamitrisable.java
@@ -3,15 +3,15 @@ package de.st_ddt.crazyutil.paramitrisable;
 import java.util.Comparator;
 import java.util.Map;
 
-public class SortParamitrisable<S> extends MapParamitrisable<Comparator<S>>
+public class SortParamitrisable<S> extends MapParamitrisable<Comparator<? super S>>
 {
 
-	public SortParamitrisable(final Map<String, ? extends Comparator<S>> values, final Comparator<S> defaultValue)
+	public SortParamitrisable(final Map<String, ? extends Comparator<? super S>> values, final Comparator<? super S> defaultValue)
 	{
 		super("Sorter", values, defaultValue);
 	}
 
-	public SortParamitrisable(final Map<String, ? extends Comparator<S>> values, final Comparator<S> defaultValue, final boolean lowercase)
+	public SortParamitrisable(final Map<String, ? extends Comparator<? super S>> values, final Comparator<? super S> defaultValue, final boolean lowercase)
 	{
 		super("Sorter", values, defaultValue, lowercase);
 	}

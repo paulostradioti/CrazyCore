35fdde4aa478cb46ef8aeea023b681fa9a2189f2
ST-DDT <ST-DDT@gmx.de>
2014-06-19 12:35:08 +0200

    Improved Random Util

--- src/main/java/de/st_ddt/crazyutil/RandomUtil.java
+++ src/main/java/de/st_ddt/crazyutil/RandomUtil.java
@@ -1,5 +1,6 @@
 package de.st_ddt.crazyutil;
 
+import java.util.List;
 import java.util.Random;
 
 public class RandomUtil
@@ -12,11 +13,19 @@ public class RandomUtil
 	}
 
 	@SafeVarargs
-	public static <E> E randomElement(E... elements)
+	public static <E> E randomElement(final E... elements)
 	{
 		if (elements.length == 0)
 			return null;
 		else
 			return elements[RANDOM.nextInt(elements.length - 1)];
+	}
+
+	public static <E> E randomElement(final List<E> elements)
+	{
+		if (elements.isEmpty())
+			return null;
+		else
+			return elements.get(RANDOM.nextInt(elements.size() - 1));
 	}
 }

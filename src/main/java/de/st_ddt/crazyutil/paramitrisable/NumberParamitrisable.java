bcb4b48a0b6f5fe2c64224d68e2b28c722e9b97d
ST-DDT <ST-DDT@gmx.de>
2014-06-22 12:25:41 +0200

    Improved NumberParamitrisable

--- src/main/java/de/st_ddt/crazyutil/paramitrisable/NumberParamitrisable.java
+++ src/main/java/de/st_ddt/crazyutil/paramitrisable/NumberParamitrisable.java
@@ -7,4 +7,34 @@ public abstract class NumberParamitrisable<S extends Number> extends TypedParami
 	{
 		super(defaultValue);
 	}
+
+	public S getValue(final S def)
+	{
+		return value == null ? def : getValue();
+	}
+
+	public byte getValueAByte(final byte def)
+	{
+		return value == null ? def : getValue().byteValue();
+	}
+
+	public int getValueAsInteger(final int def)
+	{
+		return value == null ? def : getValue().intValue();
+	}
+
+	public long getValueAsInteger(final long def)
+	{
+		return value == null ? def : getValue().longValue();
+	}
+
+	public float getValueAsInteger(final float def)
+	{
+		return value == null ? def : getValue().floatValue();
+	}
+
+	public double getValueAsInteger(final double def)
+	{
+		return value == null ? def : getValue().doubleValue();
+	}
 }

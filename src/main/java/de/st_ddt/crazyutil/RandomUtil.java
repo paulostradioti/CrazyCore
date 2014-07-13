711a95463715b95accfefc563aa68a86ee09cb29
ST-DDT <ST-DDT@gmx.de>
2014-07-13 09:17:27 +0200

    Improved RandomUtil

--- src/main/java/de/st_ddt/crazyutil/RandomUtil.java
+++ src/main/java/de/st_ddt/crazyutil/RandomUtil.java
@@ -1,5 +1,6 @@
 package de.st_ddt.crazyutil;
 
+import java.util.Collection;
 import java.util.List;
 import java.util.Random;
 
@@ -15,15 +16,24 @@ public class RandomUtil
 	@SafeVarargs
 	public static <E> E randomElement(final E... elements)
 	{
-		if (elements.length == 0)
+		if (elements == null || elements.length == 0)
 			return null;
 		else
 			return elements[RANDOM.nextInt(elements.length)];
 	}
 
+	@SuppressWarnings("unchecked")
+	public static <E> E randomElement(final Collection<E> elements)
+	{
+		if (elements == null || elements.isEmpty())
+			return null;
+		else
+			return (E) randomElement(elements.toArray());
+	}
+
 	public static <E> E randomElement(final List<E> elements)
 	{
-		if (elements.isEmpty())
+		if (elements == null || elements.isEmpty())
 			return null;
 		else
 			return elements.get(RANDOM.nextInt(elements.size()));
@@ -31,7 +41,10 @@ public class RandomUtil
 
 	public static int random(final int min, final int max)
 	{
-		return RANDOM.nextInt(max - min + 1) + min;
+		if (min < 0)
+			return (int) Math.abs(RANDOM.nextLong()) % (max - min + 1) + min;
+		else
+			return RANDOM.nextInt(max - min + 1) + min;
 	}
 
 	public static long random(final long min, final long max)
